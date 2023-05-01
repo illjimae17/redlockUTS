0c959149b79ad93d325522b7ffff88a1890fe6b9
Agustinus Leonardo Dwitama <agustinus.dwitama@binus.ac.id>
2023-05-01 16:50:13 +0700

    update keduah :P

--- Website/index.php
+++ Website/index.php
@@ -22,13 +22,17 @@ if (mysqli_num_rows($result) > 0) {
     // Output table header
     echo "<table><tr><th>ID(PK)</th><th>Nama</th><th>Alamat</th><th>Jabatan</th></tr>";
 
+    $a = 0;
     // Output table rows
     while ($row = mysqli_fetch_assoc($result)) {
+        $a = $a+1;
         echo "<tr><td>" . $row['ID'] . "</td><td>" . $row['Nama'] . "</td><td>" . $row['Alamat'] . "</td><td>" . $row['Jabatan'] . "</td></tr>";
+
     }
 
     // Close table
     echo "</table>";
+    echo "<p> Total User : ".$a."</p>";
 } else {
     echo "No data found";
 }
